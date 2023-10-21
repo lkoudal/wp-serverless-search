6971e1cb4585e6f2ff3576b66a634a6239413113
Erudition <cnnr_dhrty@live.com>
2023-10-20 19:04:09 -0500

    try custom hook

--- wp-serverless-search.php
+++ wp-serverless-search.php
@@ -48,10 +48,10 @@ function create_wp_sls_dir()
 //add_action('wp_loaded', 'create_search_feed');
 //add_action('publish_page', 'create_search_feed');
 //add_action('publish_post', 'create_search_feed');
-add_action('transition_post_status', 'create_search_feed');
+add_action('publish_course', 'create_search_feed');
 
 
-function create_search_feed($newstatus, $oldstatus, $post)
+function create_search_feed()
 {
 
   require_once(ABSPATH . 'wp-admin/includes/export.php');
